cb583f8e616f29c4a52c063baccf2d1b4c7ef38e
Andre Kautsar <ktsr.andre@gmail.com>
2021-09-20 16:30:38 +0700

    Add react-router-dom

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -9,7 +9,7 @@ function Example() {
                     <div className="card">
                         <div className="card-header">Example Component</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">This is an example component</div>
                     </div>
                 </div>
             </div>
@@ -19,6 +19,6 @@ function Example() {
 
 export default Example;
 
-if (document.getElementById('example')) {
-    ReactDOM.render(<Example />, document.getElementById('example'));
+if (document.getElementById('app')) {
+    ReactDOM.render(<Example />, document.getElementById('app'));
 }
